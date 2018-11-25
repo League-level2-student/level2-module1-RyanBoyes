7270552cd83cdbd7ee76c536cd75c94251ff6021
RyanBoyes <league@WTS14.attlocal.net>
2018-11-25 11:26:52 -0800

    Progress!!
    pt.2

--- src/intro_to_array_lists/LIObjectManager.java
+++ src/intro_to_array_lists/LIObjectManager.java
@@ -1,10 +1,11 @@
 package intro_to_array_lists;
 
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class LIObjectManager {
-
 	LIRocketship R1;
+	ArrayList<LIProjectile> Projectiles = new ArrayList<LIProjectile>();
 
 	LIObjectManager(LIRocketship R1) {
 		this.R1 = R1;
@@ -12,10 +13,18 @@ public class LIObjectManager {
 
 	void update() {
 		R1.update();
+		for (int i = 0; i < Projectiles.size(); i++) {
+			Projectiles.get(i);
+
+		}
 	}
 
 	void draw(Graphics g) {
 		R1.draw(g);
+	}
+
+	void addProjectile(LIProjectile Projectile) {
+		Projectiles.add(Projectile);
 	}
 
 }
--- src/intro_to_array_lists/LIProjectile.java
+++ src/intro_to_array_lists/LIProjectile.java
@@ -1,5 +1,27 @@
 package intro_to_array_lists;
 
-public class LIProjectile {
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class LIProjectile extends LIGameObject {
+
+	int speed;
+
+	LIProjectile(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		speed = 10;
+	}
+
+	void update() {
+		y = speed;
+		if (y < 0) {
+			isAlive = false;
+		}
+	}
+
+	void draw(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillRect(x, y, width, height);
+	}
 
 }
