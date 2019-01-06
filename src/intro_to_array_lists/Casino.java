e78af5dbc60d4a4367c6c0b1568ae994c56d64ff
RyanBoyes <league@WTS14.attlocal.net>
2019-01-06 11:29:17 -0800

    and more
    dah

--- src/intro_to_array_lists/Casino.java
+++ src/intro_to_array_lists/Casino.java
@@ -3,10 +3,13 @@ package intro_to_array_lists;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Casino {
 	JFrame frame;
-
+	JPanel panel;
+	JLabel Rlabel;
 	final static int width = 500;
 	final static int height = 800;
 	CGamePanel GP;
@@ -30,6 +33,10 @@ public class Casino {
 		frame.setVisible(true);
 		frame.setSize(width, height);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		panel.add(Rlabel);
+		Rlabel.setVisible(true);
+		Rlabel.addMouseListener(this);
 
 	}
 
