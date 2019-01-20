2def137d67941c31443cb4a2b23aeae0efd65cef
runxue00 <league@WTS14.attlocal.net>
2019-01-20 11:28:25 -0800

    C.GRID
    sugoisrgsugsbrjgossgnshrjglksg sjrbgkj4wltnbtrnrkgjwk

--- src/intro_to_array_lists/Casino.java
+++ src/intro_to_array_lists/Casino.java
@@ -1,15 +1,22 @@
 package intro_to_array_lists;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Casino {
+public class Casino implements ActionListener {
 	JFrame frame;
 	JPanel panel;
-	JLabel Rlabel;
+	JButton Rbutton;
+	JButton Button2;
+	JButton Button3;
 	final static int width = 500;
 	final static int height = 800;
 	CGamePanel GP;
@@ -21,6 +28,7 @@ public class Casino {
 
 	Casino() {
 		frame = new JFrame();
+		panel = new JPanel();
 
 		GP = new CGamePanel();
 	}
@@ -34,10 +42,35 @@ public class Casino {
 		frame.setSize(width, height);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(panel);
-		panel.add(Rlabel);
-		Rlabel.setVisible(true);
-		Rlabel.addMouseListener(this);
+		panel.setVisible(true);
+		panel.setBackground(Color.BLACK);
+		panel.setBounds(0, 0, width, height);
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = 10;
+		c.gridy = 600;
 
+		panel.add(Rbutton = new JButton("ROULETTE"), c);
+		Rbutton.addActionListener(this);
+		c.gridx = 200;
+		c.gridy = 600;
+
+		panel.add(Button2 = new JButton("Game 2"), c);
+		Button2.addActionListener(this);
+		c.gridx = 450;
+		c.gridy = 600;
+
+		panel.add(Button3 = new JButton("Game 3"), c);
+		Button3.addActionListener(this);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == Rbutton) {
+			System.out.println("HI");
+		}
 	}
 
 }
