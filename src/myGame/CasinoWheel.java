1f5016e1cf8bb08a4b593dac1204c273b6a8f1c0
RyanBoyes <league@WTS14.attlocal.net>
2019-05-05 11:28:09 -0700

    rdthtfgbhstb
    jh

--- src/myGame/CasinoWheel.java
+++ src/myGame/CasinoWheel.java
@@ -1,34 +1,44 @@
 package myGame;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Image;
+import java.awt.Transparency;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-public class CasinoWheel extends BufferedImage {
-	public static BufferedImage wheelImg;
-	
-	
-	public CasinoWheel(int width, int height, int imageType) {
-		super(width, height, imageType);
-		// TODO Auto-generated constructor stub
-	}
-
-
-
-
-
-
-
-
+public class CasinoWheel {
+public BufferedImage wheelImg;
 	
 	
 	
-	
-	
-	
+public static BufferedImage rotate(BufferedImage image, double angle) {
+    double sin = Math.abs(Math.sin(angle)), cos = Math.abs(Math.cos(angle));
+    int w = image.getWidth(), h = image.getHeight();
+    int neww = (int)Math.floor(w*cos+h*sin), newh = (int) Math.floor(h * cos + w * sin);
+    GraphicsConfiguration gc = getDefaultConfiguration();
+    BufferedImage result = gc.createCompatibleImage(neww, newh, Transparency.TRANSLUCENT);
+    Graphics2D g = result.createGraphics();
+    g.translate((neww - w) / 2, (newh - h) / 2);
+    g.rotate(angle, w / 2, h / 2);
+    g.drawRenderedImage(image, null);
+    g.dispose();
+    return result;
+}
+
+private static GraphicsConfiguration getDefaultConfiguration() {
+    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    GraphicsDevice gd = ge.getDefaultScreenDevice();
+    return gd.getDefaultConfiguration();
+}
+
+
+
 	
 	void drawWheel(Graphics g) {
 		g.drawImage(wheelImg, 0,0,500,800, null);
