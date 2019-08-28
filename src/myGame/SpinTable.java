0936b600059271ef13a0ca0d0b895150a9d0ade3
RyanBoyes <ryanboyes04@gmail.com>
2019-08-27 22:20:39 -0700

    tryhry
    ryry

--- src/myGame/SpinTable.java
+++ src/myGame/SpinTable.java
@@ -62,23 +62,43 @@ public void mouseClicked(MouseEvent e) {
 		}
 		
 		if(Sobj.betClicked.getText().equals("Red")) {
-			if(Sobj.NumberRolled==1) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==3) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==5) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==7) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==9) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==12) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==14) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==16) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==16) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==18) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==19) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==21) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==23) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==27) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==30) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==32) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==34) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==36) { System.out.println("WINNER"); }
+			if(Sobj.NumberRolled==1) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==3) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==5) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==7) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==9) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==12) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==14) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==16) { System.out.println("WINNER"); }  else if(Sobj.NumberRolled==18) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==19) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==21) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==23) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==27) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==30) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==32) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==34) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==36) { System.out.println("WINNER"); }
+		}
+			else {
+				System.out.println("");
+			}
+		
+		
+		if (Sobj.betClicked.getText().equals("Black")) {
+			if(Sobj.NumberRolled==2) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==4) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==6) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==8) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==10) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==11) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==13) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==15) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==17) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==20) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==22) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==24) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==25) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==26) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==28) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==29) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==31) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==33) { System.out.println("WINNER"); } else if(Sobj.NumberRolled==35 ) { System.out.println("WINNER"); } 
+		
+		else {
+			System.out.println("");
+		}
+		}
+		if (Sobj.betClicked.getText().equals("1-18")) {
+			if(Sobj.NumberRolled<19) { System.out.println("WINNER");
+			}
+			else { System.out.println(""); }
+		}
+		
+		if(Sobj.betClicked.getText().equals("19-36")) {
+			if(Sobj.NumberRolled>18) { System.out.println("WINNER");}
 			else {
 				System.out.println("");
 			}
 		}
 		
-		if (Sobj.betClicked.getText().equals("Black")) {
-			
+		if(Sobj.betClicked.getText().equals("1")) {
+			if(Sobj.NumberRolled==1) { System.out.println("WINNER");}
+			else {
+				System.out.println("");
+			}
 		}
 		
 		
 		
 		
-		
-		if(Sobj.NumberRolled == Sobj.betClickedI) {
-			System.out.println("WINNER");
-		}
 	}
 	
 
